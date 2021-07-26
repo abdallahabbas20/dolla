f6e7701fc25436b51d2344665c32332521f4c58c
Abdallah Abbas <abdallahabbas@Abdallahs-iMac.local>
2021-07-26 23:33:06 +0100

    Mobile autoplay

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -13,7 +13,7 @@ const HeroSection = () => {
     return (
         <HeroContainer>
             <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+                <VideoBg autoPlay loop muted playsInline src={Video} type='video/mp4' />
             </HeroBg>
             <HeroContent>
                 <HeroH1>Virtual Banking Made Easy</HeroH1>
